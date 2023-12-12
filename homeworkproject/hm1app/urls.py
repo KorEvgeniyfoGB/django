28f5a6eb075f832d059ede36f5ad18193b54853a
KorEvgeniyfoGB <Bagz.bann4-l011a.Bann4@yandex.by>
2023-12-12 18:53:22 +0300

    homework 003

--- homeworkproject/hm1app/urls.py
+++ homeworkproject/hm1app/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from .views import index, about
 
 urlpatterns = [
-    path('/', index, name='index'),
-    path('/about/', about, name='about'),
+    path('', index, name='index'),
+    path('about/', about, name='about'),
 
 ]
--- homeworkproject/hm2app/views.py
+++ homeworkproject/hm2app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from datetime import timedelta, datetime
+from datetime import timedelta
 from django.utils import timezone
 from .models import Order
 
 
 def get_client_orders(request, client_id):
-    today = datetime.now()
+    today = timezone.now()
     orders_last_week = Order.objects.filter(
         client=client_id,
         date_ordered__gte=today - timedelta(days=7)).distinct()
