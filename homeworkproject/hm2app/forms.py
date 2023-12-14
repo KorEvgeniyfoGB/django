a2306043dd0ee03b7c76ad5e41309dddb9e8d0ce
KorEvgeniyfoGB <Bagz.bann4-l011a.Bann4@yandex.by>
2023-12-14 21:22:40 +0300

    homework 004

--- homeworkproject/hm2app/forms.py
+++ homeworkproject/hm2app/forms.py
@@ -6,5 +6,8 @@ class AddProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'description', 'photo', 'price', 'quantity']
-        widgets = {'description': forms.Textarea(attrs={'cols': 50, 'rows': 5, 'style': 'resize: none'})}
+        widgets = {'description': forms.Textarea(attrs={'cols': 50, 'rows': 5,
+                                                        'style': 'resize: none; '
+                                                                 'background-color: '
+                                                                 '#f8f8f8; border-radius: 4px;'})}
 
